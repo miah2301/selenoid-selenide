b3b15506cdec48cf89f863087e0dc577abd5336c
Mihail Seregin <mihail.mieke@yandex.ru>
2023-02-12 18:36:29 +0300

    111

--- src/test/java/org/selenide/examples/selenoid/SelenoidExtension.java
+++ src/test/java/org/selenide/examples/selenoid/SelenoidExtension.java
@@ -11,7 +11,7 @@ import java.util.Map;
 class SelenoidExtension implements BeforeAllCallback  {
   @Override
   public void beforeAll(ExtensionContext context) {
-    Configuration.remote = "http://localhost:4444/wd/hub";
+    Configuration.remote = "http://192.168.1.115:4444/wd/hub";
     Configuration.reportsFolder = "target/surefire-reports";
     Configuration.downloadsFolder = "target/downloads";
 
